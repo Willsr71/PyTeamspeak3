6cad71ad85d335dedf74494b2c0b4f2d2bc460bd
Willsr71 <willsr71@gmail.com>
2016-05-06 19:14:28 -0400

    Config options for sectioned backups

--- backup.py
+++ backup.py
@@ -9,28 +9,33 @@ config = teamspeak.get_json_file("configenjin.json")
 tn = teamspeak.connect(config["host"], config["queryport"], config["port"], config["user"], config["password"], "TSBackup")
 
 # Server Info
-backup_data["server"] = teamspeak.server_info(tn)
+if config["backup"]["backup_server_info"]:
+    backup_data["server"] = teamspeak.server_info(tn)
 
 # Channels
-backup_data["channels"] = teamspeak.channel_list(tn)
+if config["backup"]["backup_channels"]:
+    backup_data["channels"] = teamspeak.channel_list(tn)
 
 # Bans
-backup_data["bans"] = teamspeak.ban_list(tn)
+if config["backup"]["backup_bans"]:
+    backup_data["bans"] = teamspeak.ban_list(tn)
 
 # Server Groups
-server_groups = teamspeak.server_group_list(tn)
-for server_group in server_groups:
-    server_group["permissions"] = teamspeak.server_group_permission_list(tn, server_group["sgid"], config["use_permission_string_ids"])
+if config["backup"]["backup_server_groups"]:
+    server_groups = teamspeak.server_group_list(tn)
+    for server_group in server_groups:
+        server_group["permissions"] = teamspeak.server_group_permission_list(tn, server_group["sgid"], config["json"]["use_permission_string_ids"])
 
-backup_data["server_groups"] = server_groups
+    backup_data["server_groups"] = server_groups
 
 # Channel Groups
-channel_groups = teamspeak.channel_group_list(tn)
-for channel_group in channel_groups:
-    channel_group["permissions"] = teamspeak.channel_group_permission_list(tn, channel_group["cgid"], config["use_permission_string_ids"])
+if config["backup"]["backup_channel_groups"]:
+    channel_groups = teamspeak.channel_group_list(tn)
+    for channel_group in channel_groups:
+        channel_group["permissions"] = teamspeak.channel_group_permission_list(tn, channel_group["cgid"], config["json"]["use_permission_string_ids"])
 
-backup_data["channel_groups"] = channel_groups
+    backup_data["channel_groups"] = channel_groups
 
-file = teamspeak.set_json_file("backup-" + str(timestamp) + ".json", backup_data, config["json_file_indents"])
+file = teamspeak.set_json_file("backup-" + str(timestamp) + ".json", backup_data, config["json"]["use_file_indentation"])
 teamspeak.quit(tn)
 tn.close()
