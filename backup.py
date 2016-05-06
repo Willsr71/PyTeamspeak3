25ae426acf76ff095b77241a76b7415f7d6d78eb
Willsr71 <willsr71@gmail.com>
2016-05-06 16:31:39 -0400

    Backup bans

--- backup.py
+++ backup.py
@@ -12,9 +12,24 @@ tn = teamspeak.connect(config["host"], config["queryport"], config["port"], conf
 # buf = teamspeak.send_command(tn, "help")
 # print(buf)
 
-bans = teamspeak.ban_list(tn)
-print(bans)
+# Bans
 
-# file = teamspeak.set_json_file("backup-" + str(timestamp) + ".json", backup_data)
+bans = []
+ban_list = teamspeak.ban_list(tn).split("|")
+for ban_listing in ban_list:
+    ban_listing = ban_listing.split(" ")
+    ban = {}
+
+    for ban_info in ban_listing:
+        if "=" in ban_info:
+            ban_info = ban_info.split("=")
+            ban[ban_info[0]] = ban_info[1]
+        else:
+            ban[ban_info] = None
+
+    bans.append(ban)
+backup_data["bans"] = bans
+
+file = teamspeak.set_json_file("backup-" + str(timestamp) + ".json", backup_data)
 teamspeak.quit(tn)
 tn.close()
